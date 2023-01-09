580ef54d2bc99a7ba68710335627bfe3c7ba7fd8
ptejas26 <ptejas.patelia@gmail.com>
2023-01-08 21:35:06 -0500

    Made a change

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = "default value"
-    var content: String = ""
+    var title:   String = "Hello"
+    var content: String = "There"
     
     var id: String = UUID().uuidString
     
